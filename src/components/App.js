d162b41f949ac72dfb9c1322ea1b0a843f503287
Danielle_M <daniellematalonis@gmail.com>
2021-09-15 21:30:48 -0400

    updated SearchBar

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,21 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-   async onSearchSubmit (term){
-        await axios.get('https://api.unsplash.com/search/photos', {
+   async onSearchSubmit (term) {
+       const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term } ,
             headers: {
                 Authorition: 'Client-ID aMvqHogjcGjL3gUufxV-27MJSGeQz3PGI-gRTw_pq7o'
             }
-         })
-         .then(response => {
-             console.log(response);
          });
-    }
+    
+             console.log(response.data.results);
+         }
 
     render () {
         return (
             <div className= "ui container" style= {{marginTop: "2%"}}>
-                <h2>Image Search</h2>
+            
                 <SearchBar onSubmit={this.onSearchSubmit} />
             
             </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,24 +4,28 @@ class SearchBar extends React.Component {
    state = {term: ''};
 
    onFormSubmit = event => {
+       event.preventDefault();
        this.props.onSubmit(this.state.term);
    };
 
     render() {
         return (
-            <div className= "ui input">
+            <div className= "ui segment">
                 <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
                     <input 
                     type= "text" 
                     value={this.state.term}
                     placeholder= "Search..." 
-                    onChange= {event => this.setState({ term: event.target.value })}
+                    onChange= {e => this.setState({ term: e.target.value })}
                     /> 
+                    </div>
                 </form>
             </div>
      
         );
-        }
-};
+    }
+}
 
 export default SearchBar;
